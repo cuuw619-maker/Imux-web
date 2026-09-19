72ae4329f680926f1903961a8a68e84a64cb15e5
cuuw619-maker <cuuw619@gmail.com>
2026-09-19 19:18:24 +0300

    Add textured block registry and hotbar

--- src/blocks.js
+++ src/blocks.js
@@ -1,1 +1,32 @@
-export const BLOCKS={grass:{color:0x55aa45},dirt:{color:0x8b5a32},stone:{color:0x888888},wood:{color:0x9a6b3f}};
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js";
+
+const asset=name=>new URL(`../${name}`,import.meta.url).href;
+
+export const BLOCKS={
+  bedrock:{texture:"bedrock.png",breakable:false},
+  stone:{texture:"stone.png",breakable:true},
+  dirt:{texture:"dirt.png",breakable:true},
+  grass:{texture:"dirt.png",color:0x75a94c,breakable:true},
+  cobblestone:{texture:"cobblestone.png",breakable:true},
+  coarse_dirt:{texture:"coarse_dirt.png",breakable:true},
+  rooted_dirt:{texture:"rooted_dirt.png",breakable:true},
+  dirt_path:{texture:"dirt_path_top.png",breakable:true}
+};
+
+export const HOTBAR=["grass","dirt","stone","cobblestone","coarse_dirt","rooted_dirt","dirt_path","stone","dirt"];
+
+export function createMaterials(){
+  const loader=new THREE.TextureLoader();
+  const materials={};
+  for(const [name,block] of Object.entries(BLOCKS)){
+    const texture=loader.load(asset(block.texture));
+    texture.colorSpace=THREE.SRGBColorSpace;
+    texture.magFilter=THREE.NearestFilter;
+    texture.minFilter=THREE.NearestMipmapLinearFilter;
+    texture.generateMipmaps=true;
+    materials[name]=new THREE.MeshLambertMaterial({map:texture,color:block.color??0xffffff});
+  }
+  return materials;
+}
+
+export function textureUrl(name){return asset(BLOCKS[name].texture);}
